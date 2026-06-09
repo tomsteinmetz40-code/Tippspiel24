d1ffad5f7aa8e705103c61d04267d63f27be376c
tomsteinmetz40-code <tomsteinmetz40@gmail.com>
2026-06-09 18:15:19 +0200

    change 1

--- main.py
+++ main.py
@@ -550,6 +550,8 @@ def datum_filter(s):
     return dt.strftime("%a, %d.%m.%Y %H:%M")
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
